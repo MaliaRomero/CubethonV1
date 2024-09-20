8924d5563388a4a1a40d9301cf32366875eade9b
MaliaRomero <romerm15@my.erau.edu>
2024-09-20 12:25:38 -0700

    Adding Instant Replay

--- Cubethon - WorkingCopy1/Assets/Scripts/PlayerMovement.cs
+++ Cubethon - WorkingCopy1/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,13 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody rb;
     public float forwardForce = 2000f;
     public float sidewaysForce = 500f;
+    //For command pattern
+    GameManager gameManager;
+
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
 
     void FixedUpdate()
     {
@@ -11,17 +18,30 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetKey("d"))
         {
-            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            // rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
+            // instead of just calling addforce, we want to package this up as a command
+            // and send to an invoker
+            // we'll need a command class, some commands, and an invoker...
+            Command moveRight = new MoveRight(rb, sidewaysForce);
+            Invoker invoker = new Invoker();
+            invoker.SetCommand(moveRight);
+            invoker.ExecuteCommand();
         }
 
         if (Input.GetKey("a"))
         {
-            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            //rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            Command moveLeft = new MoveLeft(rb, sidewaysForce);
+            Invoker invoker = new Invoker();
+            invoker.SetCommand(moveLeft);
+            invoker.ExecuteCommand();
         }
 
         if (rb.position.y < -1f)
         {
-            FindObjectOfType<GameManager>().EndGame();
+            FindObjectOfType<GameManager>().EndGame(null);
+            //FindObjectOfType<GameManager>().EndGame();
         }
     }
 }
